a70d0e570fdf6d8eb30e9d9d5bc933d9f3541219
MichaelFKogan <mfkogan@gmail.com>
2016-11-17 15:34:34 -0500

    added MYSQL schema and seed files

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name,devoured) 
+VALUES ("Whopper Senior",TRUE);
+
+INSERT INTO burgers (burger_name,devoured) 
+VALUES ("Grand Whopper",FALSE);
+
+INSERT INTO burgers (burger_name,devoured) 
+VALUES ("The Mac Burger",FALSE);
+
+
