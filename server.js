29a8184b250566ad0aa22ae832e451ea0e9848bb
MichaelFKogan <mfkogan@gmail.com>
2016-11-17 15:32:38 -0500

    added jaws db to server.js for heroku db

--- server.js
+++ server.js
@@ -19,12 +19,18 @@ app.set('view engine', 'handlebars');
 //==========================================================
 
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL){
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'burgers_db'
 });
+}
 
 connection.connect(function(err) {
   if (err) {
